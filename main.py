0bb4708015f3dea2186dabe16f265692e0b11fc4
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-14 14:38:57 +0100

    .gitignore initializtion

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0)) #Black:(0,0,0)
+        screen.fill((0,0,0)) 
         pygame.display.flip()
 
 if __name__ == "__main__":
