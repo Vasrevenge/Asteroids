7706c8fd3f0bb2003085f1839a4deea9a0b3f061
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-14 16:12:31 +0100

    Player movement

--- main.py
+++ main.py
@@ -5,10 +5,12 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2),PLAYER_RADIUS) #(SCREEN_WIDTH/2), (SCREEN_HEIGHT/2),PLAYER_RADIUS
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -20,10 +22,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0)) 
+        screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.tick(60)/1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
