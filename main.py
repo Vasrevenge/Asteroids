a67dd76d81e491cf9c6717881f94120c716a6c0e
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-14 14:37:28 +0100

    venv correction

--- main.py
+++ main.py
@@ -3,10 +3,23 @@
 # throughout this file
 
 import pygame
+from pygame.locals import *
 from constants import *
 
 def main():
-    print("Starting Asteroids")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print('Starting Asteroids!')
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0)) #Black:(0,0,0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
